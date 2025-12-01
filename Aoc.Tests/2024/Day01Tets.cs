cffd3180131a62f8f5bed28973fa290a9d8a7f09
ElisaAshryver <elisaashryver@hotmail.com>
2025-12-01 23:04:25 +0100

    Refactor Day01 solution to improve part calculation and add unit tests for part 2

--- Aoc.Tests/2024/Day01Tets.cs
+++ Aoc.Tests/2024/Day01Tets.cs
@@ -5,12 +5,21 @@ namespace Aoc.Tests._2024;
 public class Day01Tets
 {
     private readonly Aoc.Solutions._2024.Day01 _solution = new();
+    private readonly string _filePath = $"{AppContext.BaseDirectory}/inputs/2024/Day01.txt";
 
     [Fact]
     public void SolvePart1()
     {
-        var input = File.ReadAllLines($"{AppContext.BaseDirectory}/inputs/2024/Day01.txt");
+        var input = File.ReadAllLines(_filePath);
         var solvePart1 = _solution.SolvePart1(input);
         solvePart1.Should().Be("11");
+    }
+    
+    [Fact]
+    public void SolvePart2()
+    {
+        var input = File.ReadAllLines(_filePath);
+        var solvePart1 = _solution.SolvePart2(input);
+        solvePart1.Should().Be("31");
     }
 }
--- Aoc/Solutions/2024/Day01.cs
+++ Aoc/Solutions/2024/Day01.cs
@@ -2,26 +2,57 @@ namespace Aoc.Solutions._2024;
 
 public sealed class Day01 : IAocSolution
 {
+    private readonly List<int> _leftPart = [];
+    private readonly List<int> _rightPart = [];
+    
     public string SolvePart1(string[] input)
     {
-        var leftPart = new List<int>();
-        var rightPart = new List<int>();
-
-        foreach (var line in input)
-        {
-            var parts = line.Split([' '], StringSplitOptions.RemoveEmptyEntries);
-            leftPart.Add(int.Parse(parts[0]));
-            rightPart.Add(int.Parse(parts[1]));
-        }
-
-        leftPart.Sort();
-        rightPart.Sort();
-
-        return leftPart.Select((t, i) => Math.Abs(t - rightPart[i])).Sum().ToString();
+        MappingLeftAndRightParts(input);
+        return _leftPart.Select((t, i) => Math.Abs(t - _rightPart[i])).Sum().ToString();
     }
 
     public string SolvePart2(string[] input)
     {
-        throw new NotImplementedException();
+        MappingLeftAndRightParts(input);
+        return CalculateSimilarity().ToString();
+    }
+    
+    private void Reset()
+    {
+        _leftPart.Clear();
+        _rightPart.Clear();
+    }
+    
+    private void MappingLeftAndRightParts(string[] input)
+    {
+        Reset();
+        
+        foreach (var line in input)
+        {
+            var parts = line.Split([' '], StringSplitOptions.RemoveEmptyEntries);
+            _leftPart.Add(int.Parse(parts[0]));
+            _rightPart.Add(int.Parse(parts[1]));
+        }
+
+        _leftPart.Sort();
+        _rightPart.Sort();
+    }
+    
+    private int CalculateSimilarity()
+    {
+        var rightPartFrequency = _rightPart.GroupBy(x => x)
+            .ToDictionary(g => g.Key, g => g.Count());
+
+        var similarityScore = 0;
+
+        foreach (var t in (_leftPart))
+        {
+            if (rightPartFrequency.TryGetValue(t, out var count))
+            {
+                similarityScore += count * t;
+            }
+        }
+        
+        return similarityScore;
     }
 }
